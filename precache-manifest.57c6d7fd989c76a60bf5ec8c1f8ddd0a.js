a6c6dc65e442b458552aae9c7da0a08d2ffe6724
rodgersxy <rodgersny99@gmail.com>
2020-05-05 19:43:36 +0300

    Updates

---
+++ precache-manifest.57c6d7fd989c76a60bf5ec8c1f8ddd0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f28d2322e5af18586acdb1545f6986",
+    "url": "/covid19-LiveUpdates/index.html"
+  },
+  {
+    "revision": "df9c16560c22fba23d94",
+    "url": "/covid19-LiveUpdates/static/css/main.4f514780.chunk.css"
+  },
+  {
+    "revision": "30096ac8a6604932557b",
+    "url": "/covid19-LiveUpdates/static/js/2.a5d1836f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-LiveUpdates/static/js/2.a5d1836f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9c16560c22fba23d94",
+    "url": "/covid19-LiveUpdates/static/js/main.d6618f02.chunk.js"
+  },
+  {
+    "revision": "0750b2b7ff2867082c77",
+    "url": "/covid19-LiveUpdates/static/js/runtime-main.c595e700.js"
+  },
+  {
+    "revision": "98fd40dea03d4b985903e24b6240c82c",
+    "url": "/covid19-LiveUpdates/static/media/covid2.98fd40de.png"
+  }
+]);
